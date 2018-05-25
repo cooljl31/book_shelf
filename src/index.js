5f0d4f243a3363edf1ec13835f42b955550bd8cc
jim lesperance <contact@jimlesperance.com>
2018-05-25 23:31:19 +0200

    add dependencies and folder structure

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+const App = () => {
+  return (
+    <div>
+      lO_ol
+    </div>
+  );
+};
+
+export default App;
+ReactDOM.render(<App />, document.getElementById('root'));
