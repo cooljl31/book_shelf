975d834bbe85caa778face9a9eeaf587c8710a40
jim lesperance <contact@jimlesperance.com>
2018-05-28 12:08:40 +0200

    create user

--- generator.js
+++ generator.js
@@ -3,41 +3,76 @@
 var faker = require('./faker.min');
 var request = require('request');
 
-(function myLoop (i) {
+var dotCounter = 0;
+
+(function myLoop () {
+
   setTimeout(function () {
 
-    var options  = {
+    var options = {
       method: 'POST',
-      url: 'http://localhost:3001/api/book',
-      headers: {
-        'Postman-Token': 'cc95ed9c-0b96-44f9-9d1f-ea2c59f098eb',
-        'Cache-Control': 'no-cache',
-        'Content-Type': 'application/json'
-      },
-      body: {
-        'name':faker.fake('{{name.jobTitle}}'),
-        'author':faker.fake('{{name.findName}}'),
-        'review':faker.fake('{{lorem.sentences}}'),
-        'pages':faker.fake('{{random.number}}'),
-        'rating':`${i}`,
-        'price':faker.fake('{{commerce.price}}'),
-        'ownerID': faker.fake('{{random.uuid}}')
+      url: 'http://localhost:3001/api/register',
+      headers:
+        {'Postman-Token': '70e26f5e-cff8-4a31-89d7-fee00dbfcdc1',
+          'Cache-Control': 'no-cache',
+          'Content-Type': 'application/json'},
+      body:{
+        email: faker.fake('{{internet.email}}'),
+        password: 'helloworld',
+        name: faker.fake('{{name.firstName}}'),
+        lastname: faker.fake('{{name.lastName}}')
       },
       json: true
     };
 
-    console.log(options);
-
     request(options, function (error, response, body) {
       if (error) {
-      throw new Error(error);
+        throw new Error(error);
       }
 
-      console.log(body);
+      (function myLoop1 () {
+        setTimeout(function () {
+          var options  = {
+            method: 'POST',
+            url: 'http://localhost:3001api/book',
+            headers: {
+              'Postman-Token': 'cc95ed9c-0b96-44f9-9d1f-ea2c59f098eb',
+              'Cache-Control': 'no-cache',
+              'Content-Type': 'application/json'
+            },
+            body: {
+              'name': faker.fake('{{name.jobTitle}}'),
+              'author': faker.fake('{{name.findName}}'),
+              'review': faker.fake('{{lorem.sentences}}'),
+              'pages': faker.fake('{{random.number}}'),
+              'rating': dotCounter,
+              'price': faker.fake('{{commerce.price}}'),
+              'ownerID': body.userID
+            },
+            json: true
+          };
+          console.log(body.userID, 'hell');
+
+          request(options, function (error, response, body1) {
+            if (error) {
+              throw new Error(error);
+            }
+
+            console.log(body1);
+          });
+
+          if (dotCounter++ < 1000) {
+            myLoop1();
+          }
+        }, 7000);
+      })();
+
+      if (dotCounter++ < 1000) {
+        myLoop();
+      }
+
     });
 
-    if (--i) {
-      myLoop(i);
-    }
-  }, 100);
-})(1000);
+  }, 500);
+})();
+
